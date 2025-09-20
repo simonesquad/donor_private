94edc2645d6100cf5b09bb36799aa98bf73094d6
Simone Ballard <simone.a.ballard@gmail.com>
2025-09-19 18:28:51 -0700

    added intro page txt!

--- main-gallery/src/Slick.jsx
+++ main-gallery/src/Slick.jsx
@@ -23,6 +23,19 @@ const Slick = () => {
                 <h1 className="header">WELCOME TO THE BURN!!</h1>
                 <img height='390px' width='900px' src='src/assets/IMG_1055.jpeg' />
                 <div className="container">
+                    <p>Howdy travellers of the universe! Welcome to my commemoration page of Burning Man 2025</p>
+                    <p>It was a journey of many miles from San Diego to Las Vegas to Reno and then to Black Rock City, but really it was a journey of infinite soul miles</p>
+                    <p>There are no words to describe the first moments on Playa and my first night out in the desert as the art cars emerged from space</p>
+                    <p>I camped out with the Earth Guardians and volunteered the entire week - making sure fuel arrangments were safe</p>
+                    <p>Likewise, I explored all the fascinating diversions the PLAYA had to offer, from dance parties to Iranian cuisine</p>
+                    <p>I am truly grateful to the teams that welcomed me on site and share the outcome of my seasonal crowdfundr with recipient CORE Ukraine</p>
+                    <p>I raised awareness and funds from February - August 2025 in California while travelling through Worldpackers.</p>
+                    <p>I celebrated my 32nd birthday in LA and continue to backpack through Cali towards peace!!</p>
+                    <h3>Hope we cross paths again soon#</h3>
+                    <p>Follow me at my website shared in the legacy campaign found here:</p>
+                    <a href="https://fnd.us/b2WZGf?ref=sh_2E1mj5">Burning 4 Ukraine Relief</a>
+                </div>
+                <div className="container">
                     <Slider {...settings}>
                     {Images_1.map((item) => (
                         <div key={item.id}>
