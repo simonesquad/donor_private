2f89ec09c7ea2de99282c0ecdca6cdb18a9bf0ac
Simone Ballard <simone.a.ballard@gmail.com>
2025-11-10 19:17:43 -0800

    updated file paths

--- main-gallery/src/Images_15.js
+++ main-gallery/src/Images_15.js
@@ -1,37 +1,37 @@
 const Images_15 = [
     {
         id: 1,
-        src: "src/assets/IMG_1301.jpeg",
+        src: "main-gallery/src/assets/IMG_1301.jpeg",
         alt: "this series"
     },
     {
         id: 2,
-        src: "src/assets/IMG_1319.jpeg",
+        src: "main-gallery/src/assets/IMG_1319.jpeg",
         alt: "this series"
     },
     {
         id: 3,
-        src: "src/assets/IMG_1302.jpeg",
+        src: "main-gallery/src/assets/IMG_1302.jpeg",
         alt: "this series"
     },
     {
         id: 4,
-        src: "src/assets/IMG_1298.jpeg",
+        src: "main-gallery/src/assets/IMG_1298.jpeg",
         alt: "this series"
     },
     {
         id: 5,
-        src: "src/assets/IMG_1296.jpeg",
+        src: "main-gallery/src/assets/IMG_1296.jpeg",
         alt: "this series"
     },
     {
         id: 6,
-        src: "src/assets/IMG_1288.jpeg",
+        src: "main-gallery/src/assets/IMG_1288.jpeg",
         alt: "this series"
     },
     {
         id: 7,
-        src: "src/assets/IMG_1294.jpeg",
+        src: "main-gallery/src/assets/IMG_1294.jpeg",
         alt: "this series"
     }
 ];
